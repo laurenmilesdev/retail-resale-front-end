7d43a3ded32096c684ea6d5ddd3151609897b819
Lauren <laurenmiles.dev@gmail.com>
2023-06-28 12:08:01 -0400

    Format updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
-  return <div id="home">Home page</div>;
+  return <div>Home page</div>;
 }
 
 export function getStaticProps() {
   return {
     props: {
-      title: "Home",
+      title: 'Home',
     },
   };
 }

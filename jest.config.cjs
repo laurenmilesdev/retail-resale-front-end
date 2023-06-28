43a73ffa004a99043b7aa0cef643e2e5d766ba8e
Lauren Miles <133659063+laurenmilesdev@users.noreply.github.com>
2023-06-28 12:25:04 -0400

    Initial Configuration (#1)
    * Deleted files
    
    * Updated file structure
    
    * Installed dependencies
    
    * Switched to module
    
    * Configured eslint and prettier
    
    * Configured jest
    
    * Moved favicon
    
    * Format updates
    
    * Added workflow
    
    * Added basic test for workflow

---
+++ jest.config.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  collectCoverage: true,
+  // on node 14.x coverage provider v8 offers good speed and more or less good report
+  coverageProvider: 'v8',
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!<rootDir>/out/**',
+    '!<rootDir>/.next/**',
+    '!<rootDir>/*.config.js',
+    '!<rootDir>/coverage/**',
+  ],
+  moduleNameMapper: {
+    // Handle CSS imports (with CSS modules)
+    // https://jestjs.io/docs/webpack#mocking-css-modules
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+    // Handle image imports
+    // https://jestjs.io/docs/webpack#handling-static-assets
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/tests/mocks/file-mock.js',
+
+    // Handle CSS imports (without CSS modules)
+    '\\.(css|less|sass|scss)$': '<rootDir>/tests/mocks/style-mock.js',
+
+    // Handle module aliases
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+  },
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  testEnvironment: 'jsdom',
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};

11713239cafb2d1ef3df535c4bc6af808e38634f
Lauren <laurenmiles.dev@gmail.com>
2023-06-30 12:32:55 -0400

    Updated CSS and _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,27 @@
+import { useState } from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Image from 'next/image';
+import Navigation from '../components/navigation/Navigation';
+import Footer from '../components/footer/Footer';
+import Tab from '../models/Tab';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css';
 
+import img from '../../public/images/retail-resale-logo.png';
+import styles from '../styles/App.module.css';
+
 export default function App({ Component, pageProps }: AppProps) {
+  const [pageValue, setPageValue] = useState<number>(0);
+  const pages = [
+    new Tab('Home', 'Home', <>Home</>),
+    new Tab('Products', 'Products', <>Products</>),
+  ];
+
+  function handlePageChange(event: React.SyntheticEvent, newValue: number) {
+    setPageValue(newValue);
+  }
+
   return (
     <>
       <Head>
@@ -13,7 +31,23 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/public/favicon.ico" />
       </Head>
 
-      <Component {...pageProps} />
+      <div className={styles.container}>
+        <div className="col-md-3">
+          <Image src={img} alt="Retail Resale logo" className={styles.logo} />
+        </div>
+
+        <div className="col-md-9">
+          <Navigation
+            labels={pages.map((page) => page.label)}
+            pageValue={pageValue}
+            handleChange={handlePageChange}
+          />
+
+          <Component {...pageProps} pageValue={pageValue} pages={pages} />
+        </div>
+      </div>
+
+      <Footer />
     </>
   );
 }

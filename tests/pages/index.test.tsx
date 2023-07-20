e47d5f372b1fee926cbaef5cb0f73456e1b969cd
Lauren <laurenmiles.dev@gmail.com>
2023-07-20 17:24:41 -0400

    Fixed unit test

--- tests/pages/index.test.tsx
+++ tests/pages/index.test.tsx
@@ -1,14 +1,10 @@
 import { render } from '@testing-library/react';
 
 import Home from '../../src/pages/index';
-import Tab from '../../src/models/Tab';
 
 describe('Index page', () => {
-  const pageValue = 0;
-  const pages = [new Tab('Home', 'Home', <div id="home">Home</div>)];
-
   it('should render', () => {
-    render(<Home pageValue={pageValue} pages={pages} />);
+    render(<Home />);
 
     const element = document.getElementById('home') as HTMLDivElement;
 

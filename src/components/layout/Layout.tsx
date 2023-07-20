371bdc6fd2fe3a89a51810f1f93a35384167aa71
Lauren <laurenmiles.dev@gmail.com>
2023-07-20 17:19:50 -0400

    Updated Layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,27 +1,37 @@
-import Image from 'next/image';
-import { Container } from '@mui/material';
+import { Dispatch, SetStateAction } from 'react';
+import Window from '../windows/window/Window';
+import Footer from '../footer/Footer';
+import WindowModel from '../../models/window';
 
-import styles from './Layout.module.css';
+type Props = {
+  windows: WindowModel[];
+  focusedWindow: WindowModel;
+  setFocusedWindow: Dispatch<SetStateAction<WindowModel>>;
+  menuItems: WindowModel[];
+  children?: React.ReactNode;
+};
 
-export default function Layout({ children, title }: any) {
+export default function Layout({
+  windows,
+  focusedWindow,
+  setFocusedWindow,
+  menuItems,
+  children,
+}: Props) {
   return (
-    <Container>
-      <div className={`${styles.window} col-md-12`}>
-        <div className={`${styles['title-container']} col-md-12`}>
-          <Image
-            src="http://i63.tinypic.com/117hi0p.png"
-            alt="windows"
-            width="30"
-            height="30"
-            className={styles.logo}
-          />
-          <div></div>
-          <p className={styles.title}>{title}</p>
-          <button className={styles.btn}>X</button>
-          <button className={styles.btn}>?</button>
-        </div>
-        <div className={styles.body}>{children}</div>
-      </div>
-    </Container>
+    <>
+      {windows.map((window: WindowModel) => (
+        <Window window={window} content={window.component} setFocusedWindow={setFocusedWindow} />
+      ))}
+
+      <div>{children}</div>
+
+      <Footer
+        windows={windows}
+        focusedWindow={focusedWindow}
+        setFocusedWindow={setFocusedWindow}
+        menuItems={menuItems}
+      />
+    </>
   );
 }

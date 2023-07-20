f34528ddcf33dc16473b36f62093695a40edc901
Lauren <laurenmiles.dev@gmail.com>
2023-07-20 17:22:51 -0400

    Updated index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,3 @@
-import { useEffect, useState } from 'react';
-import Layout from '../components/layout/Layout';
-import TabPanel from '../components/tab-panel/TabPanel';
-import Tab from '../models/Tab';
-
-type Props = {
-  pageValue: number;
-  pages: Tab[];
-};
-
-export default function Home({ pageValue, pages }: Props): JSX.Element {
-  const [title, setTitle] = useState<string>('Retail Resale');
-
-  useEffect(() => {
-    const page = pages[pageValue];
-
-    setTitle(page.title);
-  }, [pageValue]);
-
-  return (
-    <Layout title={title}>
-      {pages.map(({ component }, index: number) => (
-        <TabPanel value={pageValue} index={index} key={index}>
-          {component}
-        </TabPanel>
-      ))}
-    </Layout>
-  );
-}
-
-export function getStaticProps() {
-  return { props: { title: 'Lauren Miles' } };
+export default function Home() {
+  return <></>;
 }

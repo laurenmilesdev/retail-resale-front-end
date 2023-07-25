7d06f73496b6b8e2c23197f8ec14c04063181191
Lauren <laurenmiles.dev@gmail.com>
2023-07-25 17:54:46 -0400

    Updated StartBarButton

--- src/components/windows/start-bar-button/StartBarButton.tsx
+++ src/components/windows/start-bar-button/StartBarButton.tsx
@@ -1,8 +1,5 @@
 import Image from 'next/image';
-import { Button } from '@mui/material';
 import WindowModel from '@/models/window';
-
-import * as WindowUtils from '../../../utils/window';
 
 import img from '../../../../public/images/ie.png';
 import styles from './StartBarButton.module.css';
@@ -14,16 +11,11 @@ type Props = {
 
 export default function StartBarButton({ window, index }: Props) {
   return (
-    <Button
-      className={`${styles.btn} windows-box-shadow`}
-      id={window.buttonId}
-      onClick={() => WindowUtils.openCloseWindow(window, false)}
-      key={index}
-    >
+    <div className={`${styles.btn} windows-box-shadow`} id={window.buttonId} key={index}>
       <div className={styles['btn-label']}>
         <Image src={img} alt="Internet Explorer icon" className={styles['ie-icon']} />
-        {window.title}
+        <div className={styles.text}>{window.title}</div>
       </div>
-    </Button>
+    </div>
   );
 }

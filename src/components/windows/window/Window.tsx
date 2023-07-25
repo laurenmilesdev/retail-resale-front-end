c969daf259f00e35edc72410407abd80d2c15c08
Lauren <laurenmiles.dev@gmail.com>
2023-07-25 17:44:15 -0400

    Updated Window

--- src/components/windows/window/Window.tsx
+++ src/components/windows/window/Window.tsx
@@ -1,29 +1,26 @@
-import { Dispatch, SetStateAction } from 'react';
 import WindowHeader from '../window-header/WindowHeader';
 import WindowOptions from '../window-options/WindowOptions';
 import WindowAddressBar from '../window-address-bar/WindowAddressBar';
-import WindowModel from '@/models/window';
 
 import styles from './Window.module.css';
 
 type Props = {
-  window: WindowModel;
-  content: JSX.Element;
-  setFocusedWindow: Dispatch<SetStateAction<WindowModel>>;
+  title: string;
+  children?: React.ReactNode;
 };
 
-export default function Window({ window, content, setFocusedWindow }: Props) {
+export default function Window({ title, children }: Props) {
   const options = ['File', 'Edit', 'Views', 'Favorites', 'Tools', 'Help'];
 
   return (
-    <div className={`${styles.window} windows-box-shadow`} id={window.windowId}>
-      <WindowHeader window={window} setFocusedWindow={setFocusedWindow} />
+    <div className={`${styles.window} windows-box-shadow`}>
+      <WindowHeader title={title} />
 
       <WindowOptions options={options} />
 
       <WindowAddressBar />
 
-      <div className={styles.content}>{content}</div>
+      <div className={styles.content}>{children}</div>
     </div>
   );
 }

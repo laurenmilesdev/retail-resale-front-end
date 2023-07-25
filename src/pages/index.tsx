12f924b9f533a39956628ebd48828e606dec8d56
Lauren <laurenmiles.dev@gmail.com>
2023-07-25 17:44:55 -0400

    Updated index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <div id="home"></div>;
+import TabPanel from '@/components/tab-panel/TabPanel';
+import WindowModel from '@/models/window';
+
+type Props = {
+  windowValue: number;
+  windows: WindowModel[];
+};
+
+export default function Home({ windowValue, windows }: Props) {
+  return (
+    <>
+      {windows.map(({ component }, index: number) => (
+        <TabPanel value={windowValue} index={index} key={index}>
+          {component}
+        </TabPanel>
+      ))}
+    </>
+  );
 }

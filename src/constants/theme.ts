ddb87a64117db6badf6cfacd93e865dca17dfbbe
Lauren <laurenmiles.dev@gmail.com>
2023-07-26 14:38:19 -0400

    Removed unneeded files

--- src/constants/theme.ts
+++
@@ -1,5 +0,0 @@
-export default class ThemeConstants {
-  static readonly DARK: string = 'dark';
-
-  static readonly LIGHT: string = 'light';
-}
--- tests/mocks/use-state-mock.ts
+++
@@ -1,8 +0,0 @@
-import { SetStateAction } from 'react';
-
-const setState = jest.fn();
-export const useStateMock: any = (useState: any) => [useState, setState];
-
-export function setStateMock(value: SetStateAction<any>): void {
-  throw new Error('Function not implemented.');
-}

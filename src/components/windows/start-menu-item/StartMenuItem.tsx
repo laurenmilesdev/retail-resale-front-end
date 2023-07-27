e78c8650246849e3230e69b559a508749cc4a480
Lauren <laurenmiles.dev@gmail.com>
2023-07-26 20:55:59 -0400

    Fixed another path

--- src/components/windows/start-menu-item/StartMenuItem.tsx
+++ src/components/windows/start-menu-item/StartMenuItem.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import WindowModel from '@/models/window';
+import WindowModel from '../../../models/window';
 
 import * as WindowUtils from '../../../utils/window';
 

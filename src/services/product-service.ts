371acf7ca70158e55e28f0b23840bdbd624837e7
Lauren <laurenmiles.dev@gmail.com>
2023-08-01 12:03:56 -0400

    Created product-service

---
+++ src/services/product-service.ts
@@ -0,0 +1,21 @@
+import ApiService from './api-service';
+import ProductModel from '../models/products/product';
+
+export default class ProductService extends ApiService {
+  baseApiUrl: string;
+
+  constructor(baseApiUrl: string) {
+    super(baseApiUrl);
+    this.baseApiUrl = baseApiUrl;
+  }
+
+  async getProducts(config?: object): Promise<ProductModel[]> {
+    const url = `${this.baseApiUrl}/Products`;
+    const response = await super.get(url, config);
+    let products: ProductModel[] = [];
+
+    if (response.status === 200 && response.data) products = response.data as ProductModel[];
+
+    return products;
+  }
+}

f00c5c2039705b61d31af4078575cdc0810ab443
Lauren <laurenmiles.dev@gmail.com>
2023-09-24 17:58:58 -0400

    Added NavLinkModel

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,16 +1,18 @@
 import { Container } from '@mui/material';
 import Navigation from '../navigation/Navigation';
+import NavLinkModel from '../../models/nav-link';
 
 import styles from './Layout.module.css';
 
 type Props = {
+  pages: NavLinkModel[];
   children: React.ReactNode;
 };
 
-export default function Layout({ children }: Props) {
+export default function Layout({ pages, children }: Props) {
   return (
     <>
-      <Navigation />
+      <Navigation pages={pages} />
       <Container maxWidth="xl" className={styles.container}>
         {children}
       </Container>

1a360b3e8f9da51f86ef3318c2e41dfb06c4a75f
Lauren <laurenmiles.dev@gmail.com>
2023-09-26 19:09:13 -0400

    Updated category and sub model

--- src/models/products/category.ts
+++ src/models/products/category.ts
@@ -1,3 +1,12 @@
+/* eslint-disable import/no-cycle */
 import Dropdown from '../dropdown';
+import SubCategory from './sub-category';
 
-export default class Category extends Dropdown {}
+export default class Category extends Dropdown {
+  subCategories: SubCategory[];
+
+  constructor(id: number, value: string, subCategories: SubCategory[]) {
+    super(id, value);
+    this.subCategories = subCategories;
+  }
+}

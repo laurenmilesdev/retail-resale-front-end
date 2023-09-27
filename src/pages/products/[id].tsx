9bbd7ade9b0110e27510f5c226b23613a65007e9
Lauren <laurenmiles.dev@gmail.com>
2023-09-26 21:03:11 -0400

    Working on product page

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,72 +1,140 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { Button, Card, CardContent } from '@mui/material';
+import { Card, CardContent, SelectChangeEvent } from '@mui/material';
 import Loading from '../../components/loading/Loading';
+import FormActionButtons from '../../components/products/form-action-buttons/FormActionButtons';
 import ProductDetails from '../../components/products/product-details/ProductDetails';
 import ProductModel from '../../models/products/product';
 import ConditionModel from '../../models/products/condition';
+import SubCategoryModel from '../../models/products/sub-category';
+import CategoryModel from '../../models/products/category';
 import ProductService from '../../services/product-service';
 import ConditionService from '../../services/condition-service';
+import CategoryService from '../../services/category-service';
 
-const productService = new ProductService(process.env.NEXT_PUBLIC_BASE_API_URL as string);
-const conditionService = new ConditionService(process.env.NEXT_PUBLIC_BASE_API_URL as string);
+const baseApiUrl: string = process.env.NEXT_PUBLIC_BASE_API_URL ?? '';
+const productService = new ProductService(baseApiUrl);
+const conditionService = new ConditionService(baseApiUrl);
+const categoryService = new CategoryService(baseApiUrl);
 
 export default function Product() {
   const { id } = useRouter().query;
-  const [product, setProduct] = useState<ProductModel>();
-  const [conditions, setConditions] = useState<ConditionModel[]>();
   const [loaded, setLoaded] = useState<boolean>(false);
   const [edit, setEdit] = useState<boolean>(false);
+  const [product, setProduct] = useState<ProductModel>();
+  const [sizeTypeId, setSizeTypeId] = useState<number | undefined>();
+  const [categoryId, setCategoryId] = useState<number | undefined>();
+  const [categories, setCategories] = useState<CategoryModel[]>();
+  const [subCategoryId, setSubCategoryId] = useState<number | undefined>();
+  const [subCategories, setSubCategories] = useState<SubCategoryModel[]>();
+  const [conditionId, setConditionId] = useState<number | undefined>();
+  const [conditions, setConditions] = useState<ConditionModel[]>();
+
+  function handleCategoryChange(event: SelectChangeEvent) {
+    setCategoryId(event.target.value as unknown as number);
+  }
+
+  async function getProduct() {
+    try {
+      const response = await productService.getProductById(id as string);
+
+      setProduct(response);
+      setSizeTypeId(response.sizeType);
+      setCategoryId(response.subCategory.categoryId);
+      setSubCategoryId(response.subCategory.id);
+      setConditionId(response.conditionId);
+    } catch (error) {
+      // TODO: Handle error by returning error object and notification to user
+      setLoaded(true);
+    } finally {
+      setLoaded(true);
+    }
+  }
+
+  async function getCategories() {
+    try {
+      const response = await categoryService.getCategories();
+      const currentCategory = response.find((p) => p.id === categoryId);
+
+      setCategories(response);
+      setSubCategories(currentCategory?.subCategories);
+    } catch (error) {
+      // Handle error
+    }
+  }
+
+  async function getConditions() {
+    try {
+      const response = await conditionService.getConditions();
+
+      setConditions(response);
+    } catch (error) {
+      // Handle error
+    }
+  }
+
+  async function getCategory() {
+    try {
+      if (categoryId) {
+        const response = await categoryService.getCategoryById(categoryId);
+
+        setCategoryId(categoryId);
+        setSubCategories(response.subCategories);
+      }
+    } catch (error) {
+      // Handle error
+    }
+  }
 
   useEffect(() => {
-    const getProduct = async () => {
-      try {
-        const response = await productService.getProductById(id as string);
-
-        setProduct(response);
-      } catch (error) {
-        // TODO: Handle error by returning error object and notification to user
-        setLoaded(true);
-      } finally {
-        setLoaded(true);
-      }
-    };
-    const getConditions = async () => {
-      try {
-        const response = await conditionService.getConditions();
-
-        setConditions(response);
-      } catch (error) {
-        // Handle error
-      }
-    };
-
     getProduct();
+    getCategories();
     getConditions();
   }, []);
+
+  useEffect(() => {
+    getCategory();
+  }, [categoryId]);
 
   useEffect(() => {}, [edit]);
 
   return (
     <>
-      <Button href="/products" className="btn-primary">
-        Back
-      </Button>
-      <Button onClick={() => setEdit(!edit)} className="btn-primary">
-        {edit ? 'Save' : 'Edit'}
-      </Button>
-      {edit && (
-        <Button onClick={() => setEdit(!edit)} className="btn-primary">
-          Cancel
-        </Button>
+      {product && (
+        <FormActionButtons
+          edit={edit}
+          setEdit={setEdit}
+          sizeTypeId={product.sizeType}
+          setSizeTypeId={setSizeTypeId}
+          categoryId={product?.subCategory.categoryId}
+          setCategoryId={setCategoryId}
+          subCategoryId={product.subCategoryId}
+          setSubCategoryId={setSubCategoryId}
+          conditionId={product.conditionId}
+          setConditionId={setConditionId}
+        />
       )}
 
       <Loading loaded={loaded}>
         <Card>
           <CardContent>
-            {product ? (
-              <ProductDetails product={product} conditions={conditions ?? []} edit={edit} />
+            {product && sizeTypeId && categoryId && subCategoryId && conditionId ? (
+              <ProductDetails
+                product={product}
+                sizeTypeId={sizeTypeId}
+                setSizeTypeId={setSizeTypeId}
+                categoryId={categoryId}
+                handleCategoryChange={handleCategoryChange}
+                categories={categories ?? []}
+                subCategoryId={subCategoryId}
+                setSubCategoryId={setSubCategoryId}
+                subCategories={subCategories ?? []}
+                conditionId={conditionId}
+                setConditionId={setConditionId}
+                conditions={conditions ?? []}
+                edit={edit}
+              />
             ) : (
               <>Product not found.</>
             )}
@@ -75,4 +143,10 @@ export default function Product() {
       </Loading>
     </>
   );
+}
+
+export function getServerSideProps() {
+  return {
+    props: {},
+  };
 }

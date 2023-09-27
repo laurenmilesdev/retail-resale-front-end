74f93b0d8ce40dd4f47bcc75aa19fdc0e68e68a0
Lauren <laurenmiles.dev@gmail.com>
2023-09-27 16:38:53 -0400

    Added ListingSiteProduct model

---
+++ src/models/products/listing-site-product.ts
@@ -0,0 +1,41 @@
+// eslint-disable-next-line import/no-cycle
+import Product from './product';
+import ListingSite from './listing-site';
+
+export default class ListingSiteProduct {
+  id: number;
+
+  product: Product;
+
+  productId: number;
+
+  listingSite: ListingSite;
+
+  listingSiteId: number;
+
+  listedPrice: number;
+
+  dateListed?: Date;
+
+  urlToListing?: string;
+
+  constructor(
+    id: number,
+    product: Product,
+    productId: number,
+    listingSite: ListingSite,
+    listingSiteId: number,
+    listedPrice: number,
+    dateListed?: Date,
+    urlToListing?: string
+  ) {
+    this.id = id;
+    this.product = product;
+    this.productId = productId;
+    this.listingSite = listingSite;
+    this.listingSiteId = listingSiteId;
+    this.listedPrice = listedPrice;
+    this.dateListed = dateListed;
+    this.urlToListing = urlToListing;
+  }
+}

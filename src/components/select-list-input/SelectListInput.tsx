41d7dd48c6885f66d1190d883715d86d9573b43d
Lauren <laurenmiles.dev@gmail.com>
2023-09-28 12:17:16 -0400

    Created SelectListInput component

---
+++ src/components/select-list-input/SelectListInput.tsx
@@ -0,0 +1,32 @@
+import { MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import SelectListModel from '../../models/select-list';
+import DropdownModel from '../../models/dropdown';
+
+import styles from './SelectListInput.module.css';
+
+type Props = {
+  selectList: SelectListModel;
+  handleChange: (event: SelectChangeEvent) => void;
+};
+
+export default function SelectListInput({ selectList, handleChange }: Props) {
+  const { name } = selectList;
+
+  return (
+    <Select
+      value={selectList.value}
+      name={name}
+      id={`${name}-select`}
+      labelId={`${name}-select-label`}
+      variant="standard"
+      onChange={handleChange}
+      className={styles['select-field']}
+    >
+      {selectList.selectListItems.map((item: DropdownModel) => (
+        <MenuItem value={item.id} key={item.value}>
+          {item.value}
+        </MenuItem>
+      ))}
+    </Select>
+  );
+}

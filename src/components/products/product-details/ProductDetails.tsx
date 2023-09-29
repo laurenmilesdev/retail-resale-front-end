a6c98a1d0ca65c547eb41651aea55152015d4adf
Lauren <laurenmiles.dev@gmail.com>
2023-09-29 10:47:32 -0400

    Updated ProductDetails

--- src/components/products/product-details/ProductDetails.tsx
+++ src/components/products/product-details/ProductDetails.tsx
@@ -1,18 +1,19 @@
 import styles from './ProductDetails.module.css';
+import ProductFieldModel from '../../../models/product-field';
 
 type Props = {
-  productFields: any[];
+  productFields: ProductFieldModel[];
 };
 
 export default function ProductDetails({ productFields }: Props) {
   return (
     <>
-      {productFields.map((field) => (
+      {productFields.map((field: ProductFieldModel) => (
         <div className={`${styles.detail} col-md-12`} key={field.name}>
           <div className={`col-md-12`}>
             <h5>{field.name}</h5>
           </div>
-          <div className="col-md-12">{field.value}</div>
+          <div className="col-md-12">{field.currentValue}</div>
         </div>
       ))}
     </>

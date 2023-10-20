2d4f5af2e34d1a61642c32ac4dc62b9efa8a7b2f
Lauren <laurenmiles.dev@gmail.com>
2023-10-20 11:52:22 -0400

    Added createUpdateProduct method

--- src/services/product-service.ts
+++ src/services/product-service.ts
@@ -1,5 +1,6 @@
 import ApiService from './api-service';
 import ProductModel from '../models/products/product';
+import CreateUpdateProductModel from '../models/products/create-update-product';
 import { SizeType } from '../enums/size-type';
 
 export default class ProductService extends ApiService {
@@ -31,5 +32,23 @@ export default class ProductService extends ApiService {
     if (response.status === 200 && response.data) products = response.data as ProductModel[];
 
     return products;
+  }
+
+  async createUpdateProduct(
+    product: CreateUpdateProductModel,
+    id?: number,
+    config?: object
+  ): Promise<boolean> {
+    if (id) {
+      const url = `${this.baseApiUrl}/Products/${id}`;
+      const response = await super.put(url, product, config);
+
+      return !!(response.status === 204);
+    }
+
+    const url = `${this.baseApiUrl}/Products`;
+    const response = await super.post(url, product, config);
+
+    return !!(response.status === 200 && response.data);
   }
 }

a4e9acd5eaba00156d35496185d2ec08484b68c4
Lauren <laurenmiles.dev@gmail.com>
2023-12-14 13:11:01 -0500

    Updated method

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,14 @@
 import ErrorModel from './models/error';
 
 export default class Utils {
-  static formatDate(date: string) {
-    const tIndex = date.indexOf('T');
+  static formatDate(date?: string) {
+    if (date) {
+      const tIndex = date.indexOf('T');
 
-    return date.substring(0, tIndex);
+      return date.substring(0, tIndex);
+    }
+
+    return 'N/A';
   }
 
   static errorHandler(error: any) {
